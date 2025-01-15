7a34b62038c9435f171e4e25e9da9c1cab6b7a2e
Todd Schavey <tschavey@archer.com>
2025-01-15 10:44:27 -0500

    actually fixes the project rename issue

--- templates/flowbite.wasm/Program.cs
+++ templates/flowbite.wasm/Program.cs
@@ -1,7 +1,7 @@
 
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Flowbite.Wasm.ReplaceMe;
+using Flowbite.Wasm;
 using Flowbite.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
